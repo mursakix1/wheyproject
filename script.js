da4e32009519b594c320bcf87099e39fa9adcfff
XavierButt <58130954+XavierButt@users.noreply.github.com>
2025-06-21 23:13:41 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+const holes = document.querySelectorAll('.hole');
+
+window.addEventListener('scroll', () => {
+  const scrollY = window.scrollY;
+
+  holes.forEach(hole => {
+    const speed = parseFloat(hole.dataset.speed);
+    hole.style.transform = `translateY(${scrollY * speed}px)`;
+  });
+});
